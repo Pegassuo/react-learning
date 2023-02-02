6fbcdafb67e12cc925a68b575576c8ca5043ba57
Carlos Bermeo <carlos.xbr54@gmail.com>
2023-02-02 13:41:30 -0500

    Proptypes

--- src/index.js
+++ src/index.js
@@ -2,32 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
+import {Button} from "./Button";
+import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <UserCard
-      name="Carlos"
-      lastname="Bermeo"
-      age={20}
-      friends={["Juan", "Jairo"]}
-      married={false}
-      address={{ street: "Test123", city: "Test" }}
-      greet={function () {
-        alert("Hello");
-      }}
-    />
-    <UserCard
-      name="Juan"
-      lastname="Urgiles"
-      age={20}
-      friends={["Carlos", "Jairo"]}
-      married={false}
-      address={{ street: "Test123", city: "Test" }}
-      greet={function () {
-        alert("Hello");
-      }}
-    />
+    <TaskCard/>
   </>
 );
