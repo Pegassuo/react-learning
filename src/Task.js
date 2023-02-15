bbae8074664448ab609bc2708684798dad0f97d6
Carlos Bermeo <carlos.xbr54@gmail.com>
2023-02-15 13:37:19 -0500

    Adding styles

--- src/Task.js
+++ src/Task.js
@@ -1,6 +1,10 @@
-export function TaskCard(){
-    return <div>
+import './task.css'
+
+export function TaskCard({ready}){
+    return <div className='card'>
         <h1>Mi primer tarea</h1>
-        <p>Tarea realizada</p>
+        <span className={ready ? 'bg-green' : 'bg-red'}>
+            {ready ? "Tarea realizada" : "Tarea pendiente"}
+        </span>
     </div>
 }
